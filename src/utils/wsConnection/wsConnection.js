e5aab208164c91020ffd724760b5b4206b23b60f
Akashdeep Sarin <akashdeep.sarin@gmail.com>
2021-12-16 22:12:05 +0530

    basic functionality

---
+++ src/utils/wsConnection/wsConnection.js
@@ -0,0 +1,16 @@
+import socketClient from 'socket.io-client';
+
+const SERVER = 'http://localhost:5000';
+
+let socket;
+
+export const connectWithWebSocket = () => {
+    socket = socketClient(SERVER);
+
+    socket.on('connection', ()=> {
+        console.log('successfully connected with wss');
+        console.log(socket.id);
+    });
+
+
+}
